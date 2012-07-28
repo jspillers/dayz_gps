d2272d63979229c9560ef581a3e6f343cb1b3e5c
Jrod <jarrod@jtms.net>
2012-07-28 15:09:56 -0400

    google oauth tweak

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :google, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
   else
     provider :developer
   end
